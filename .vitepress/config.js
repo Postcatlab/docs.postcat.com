80cd10581d6c657aaeb930e0f15f078cba24d45c
夜鹰 <17kungfuboy@gmail.com>
2022-03-02 18:21:39 +0800

    style: add logo in nav

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,14 +1,15 @@
 module.exports = {
-  title: 'EoApi',
-  description: 'Just playing around.',
-  head: [['link', { rel: 'icon', href: '/favicon.ico' }]],
+  title: " ",
+  description: "Just playing around.",
+  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
   themeConfig: {
     //   头部导航
+    logo: "https://demo.eoapi.io/assets/images/logo.svg",
     nav: [
-      { text: '首页', link: '/index' },
-      { text: '文档', link: '/docs/about' },
-      { text: 'Demo', link: 'https://demo.eoapi.io/' },
-    ],
+      { text: "首页", link: "/index" },
+      { text: "文档", link: "/docs/about" },
+      { text: "Demo", link: "https://demo.eoapi.io/" }
+    ]
   },
-  srcExclude: ['README.md'],
-}
+  srcExclude: ["README.md"]
+};
