587e38794dc84c344a8310530df7c8fc214c953a
kungfuboy <17kungfuboy@gmail.com>
2022-05-20 15:30:14 +0800

    feat: update download link

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,100 +1,101 @@
+const version = '1.0.0'
 module.exports = {
-  lang: "zh-Hans",
+  lang: 'zh-Hans',
   locales: {
-    "/": {
-      lang: "zh-CN",
-      title: "Eoapi",
-      description: "一个可拓展的开源 API 工具",
+    '/': {
+      lang: 'zh-CN',
+      title: 'Eoapi',
+      description: '一个可拓展的开源 API 工具',
     },
-    "/en/": {
-      lang: "en-US", // 将会被设置为 <html> 的 lang 属性
-      title: "Eoapi",
-      description: "An extensible open source API tool",
+    '/en/': {
+      lang: 'en-US', // 将会被设置为 <html> 的 lang 属性
+      title: 'Eoapi',
+      description: 'An extensible open source API tool',
     },
   },
   head: [
-    ["link", { rel: "icon", href: "/favicon.ico" }],
-    ["script", { src: "/lib/medium-zoom.js" }],
-    ["script", { src: "/zoom-image.js" }],
+    ['link', { rel: 'icon', href: '/favicon.ico' }],
+    ['script', { src: '/lib/medium-zoom.js' }],
+    ['script', { src: '/zoom-image.js' }],
   ],
   lastUpdated: true,
   themeConfig: {
     algolia: {
-      appId: "DWNA8Q7OW9",
-      apiKey: "74082b5eb9ea2c675445503c70f6b4e2",
-      indexName: "eoapi",
+      appId: 'DWNA8Q7OW9',
+      apiKey: '74082b5eb9ea2c675445503c70f6b4e2',
+      indexName: 'eoapi',
     },
     locales: {
-      "/": {
-        selectText: "选择语言",
-        label: "简体中文",
-        editLinkText: "贡献此文档",
-        lastUpdated: "最后更新",
+      '/': {
+        selectText: '选择语言',
+        label: '简体中文',
+        editLinkText: '贡献此文档',
+        lastUpdated: '最后更新',
         nav: [
-          { text: "首页", link: "/index" },
-          { text: "文档", link: "/docs/about" },
-          { text: "API", link: "/api/overview" },
+          { text: '首页', link: '/index' },
+          { text: '文档', link: '/docs/about' },
+          { text: 'API', link: '/api/overview' },
           {
-            text: "下载",
-            ariaLabel: "Download",
+            text: '下载',
+            ariaLabel: 'Download',
             items: [
               {
-                text: "Windows",
-                link: "https://github.com/eolinker/eoapi/releases/download/v0.0.1-beta/eoapi-Setup-0.0.1-beta.exe",
-                target: "_self",
+                text: 'Windows',
+                link: `https://github.com/eolinker/eoapi/releases/latest/download/eoapi-Setup-${version}.exe`,
+                target: '_self',
               },
               {
-                text: "MacOS(Intel)",
-                link: "https://github.com/eolinker/eoapi/releases/download/v0.0.1-beta/eoapi-0.0.1-beta.dmg",
-                target: "_self",
+                text: 'MacOS(Intel)',
+                link: `https://github.com/eolinker/eoapi/releases/latest/download/eoapi-${version}.dmg`,
+                target: '_self',
               },
-              // {
-              //   text: "MacOS(M1)",
-              //   link: "https://github.com/eolinker/eoapi/releases/download/v0.0.1-beta/eoapi-0.0.1-beta.dmg",
-              //   target: "_self",
-              // },
+              {
+                text: 'MacOS(M1)',
+                link: `https://github.com/eolinker/eoapi/releases/latest/download/eoapi-${version}-arm64.dmg`,
+                target: '_self',
+              },
             ],
           },
-          { text: "Live Demo", link: "https://demo.eoapi.io/" },
-          { text: "Github", link: "https://github.com/eolinker/eoapi" },
+          { text: 'Live Demo', link: 'https://demo.eoapi.io/' },
+          { text: 'Github', link: 'https://github.com/eolinker/eoapi' },
         ],
         sidebar: {
-          "/docs/": [
+          '/docs/': [
             {
-              text: "产品手册",
+              text: '产品手册',
               children: [
-                { text: "产品简介", link: "/docs/about" },
+                { text: '产品简介', link: '/docs/about' },
                 {
-                  text: "基础功能",
+                  text: '基础功能',
                   children: [
-                    { text: "接口测试", link: "/docs/apitest" },
-                    { text: "接口文档", link: "/docs/apidoc" },
-                    { text: "测试环境", link: "/docs/env" },
+                    { text: '接口测试', link: '/docs/apitest' },
+                    { text: '接口文档', link: '/docs/apidoc' },
+                    { text: '测试环境', link: '/docs/env' },
                   ],
                 },
                 {
-                  text: "插件广场",
-                  link: "/docs/extensions",
+                  text: '插件广场',
+                  link: '/docs/extensions',
                   children: [
-                    { text: "推送", link: "/docs/extension-push" },
-                    { text: "导出", link: "/docs/extension-export" },
+                    { text: '推送', link: '/docs/extension-push' },
+                    { text: '导出', link: '/docs/extension-export' },
                   ],
                 },
-                { text: "常见问题", link: "/docs/FAQ" },
+                { text: '常见问题', link: '/docs/FAQ' },
               ],
             },
             {
-              text: "联系我们",
-              link: "/docs/contact",
+              text: '联系我们',
+              link: '/docs/contact',
             },
           ],
-          "/api/": [
+          '/api/': [
             {
-              text: "插件",
-              link: "/api/overview",
+              text: '插件',
+              link: '/api/overview',
               children: [
-                { text: "快速开始", link: "/api/get-started" },
-                { text: "可贡献功能点", link: "/api/feature-contribution" },
+                { text: '快速开始', link: '/api/get-started' },
+                { text: '可贡献功能点', link: '/api/feature-contribution' },
               ],
             },
             // {
@@ -104,42 +105,42 @@ module.exports = {
           ],
         },
       },
-      "/en/": {
-        selectText: "Languages",
-        label: "English",
-        editLinkText: "Contribute to this document",
+      '/en/': {
+        selectText: 'Languages',
+        label: 'English',
+        editLinkText: 'Contribute to this document',
         nav: [
-          { text: "Home", link: "/en/index" },
-          { text: "Document", link: "/en/docs/about" },
+          { text: 'Home', link: '/en/index' },
+          { text: 'Document', link: '/en/docs/about' },
           {
-            text: "Download",
-            ariaLabel: "Download",
+            text: 'Download',
+            ariaLabel: 'Download',
             items: [
               {
-                text: "MacOS",
-                link: "https://github.com/eolinker/eoapi/releases/download/v0.0.1-beta/eoapi-0.0.1-beta.dmg",
-                target: "_self",
+                text: 'MacOS',
+                link: 'https://github.com/eolinker/eoapi/releases/download/v0.0.1-beta/eoapi-0.0.1-beta.dmg',
+                target: '_self',
               },
               {
-                text: "Windows",
-                link: "https://github.com/eolinker/eoapi/releases/download/v0.0.1-beta/eoapi-Setup-0.0.1-beta.exe",
-                target: "_self",
+                text: 'Windows',
+                link: 'https://github.com/eolinker/eoapi/releases/download/v0.0.1-beta/eoapi-Setup-0.0.1-beta.exe',
+                target: '_self',
               },
             ],
           },
-          { text: "Live Demo", link: "https://demo.eoapi.io/" },
-          { text: "Github", link: "https://github.com/eolinker/eoapi" },
+          { text: 'Live Demo', link: 'https://demo.eoapi.io/' },
+          { text: 'Github', link: 'https://github.com/eolinker/eoapi' },
         ],
         sidebar: {
-          "/en/docs/": [],
+          '/en/docs/': [],
         },
       },
     },
-    docsRepo: "eolinker/eoapi-docs",
-    docsBranch: "main",
-    repo: "eolinker/eoapi",
-    logo: "/images/logo.svg",
+    docsRepo: 'eolinker/eoapi-docs',
+    docsBranch: 'main',
+    repo: 'eolinker/eoapi',
+    logo: '/images/logo.svg',
     editLinks: true,
   },
-  srcExclude: ["README.md"],
-};
+  srcExclude: ['README.md'],
+}
