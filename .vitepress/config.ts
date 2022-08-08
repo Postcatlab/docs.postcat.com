0e6bc1a3503e663c9cdac53a7facfadea7478275
夜鹰 <17kungfuboy@gmail.com>
2022-08-08 10:47:44 +0800

    feat: add extension import doc

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -6,21 +6,21 @@ export default {
     "/": {
       lang: "zh-CN",
       title: "用户使用文档 - Eoapi",
-      description: "一个可拓展的开源 API 工具",
+      description: "一个可拓展的开源 API 工具"
     },
     "/en/": {
       lang: "en-US", // 将会被设置为 <html> 的 lang 属性
       title: "Eoapi",
-      description: "An extensible open source API tool",
-    },
+      description: "An extensible open source API tool"
+    }
   },
   head: [
     ["link", { rel: "icon", href: "/favicon.ico" }],
     [
       "script",
       {
-        src: "https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/icons_10812_9.04dec48becbe244b12f6eca51b76db33.js",
-      },
+        src: "https://lf1-cdn-tos.bytegoofy.com/obj/iconpark/icons_10812_9.04dec48becbe244b12f6eca51b76db33.js"
+      }
     ],
     ["script", { src: "/lib/medium-zoom.js" }],
     ["script", { src: "/zoom-image.js" }],
@@ -34,15 +34,15 @@ export default {
   
   //custom page code end here
   
-  gio('send');`,
-    ],
+  gio('send');`
+    ]
   ],
   lastUpdated: true,
   themeConfig: {
     algolia: {
       appId: "DWNA8Q7OW9",
       apiKey: "74082b5eb9ea2c675445503c70f6b4e2",
-      indexName: "eoapi",
+      indexName: "eoapi"
     },
     locales: {
       "/": {
@@ -51,14 +51,14 @@ export default {
         editLinkText: "贡献此文档",
         lastUpdated: "最后更新",
         nav: [
-          { text: "文档", link: "/",activeMatch:'/' },
+          { text: "文档", link: "/", activeMatch: "/" },
           { text: "Live Demo", link: "https://eoapi.io/" },
           { text: "Github", link: "https://github.com/eolinker/eoapi" },
           {
             text: "下载",
             ariaLabel: "Download",
-            items: packages,
-          },
+            items: packages
+          }
         ],
         sidebar: {
           "/": [
@@ -74,37 +74,38 @@ export default {
                       link: "/docs/apitest",
                       children: [
                         { text: "前后置脚本", link: "/docs/script-function" },
-                        { text: "全局变量", link: "/docs/global-variable" },
-                      ],
+                        { text: "全局变量", link: "/docs/global-variable" }
+                      ]
                     },
                     { text: "接口文档", link: "/docs/apidoc" },
                     { text: "测试环境", link: "/docs/env" },
-                    { text: "Mock 数据", link: "/docs/mock" },
-                  ],
+                    { text: "Mock 数据", link: "/docs/mock" }
+                  ]
                 },
                 {
                   text: "插件广场",
                   link: "/docs/extensions",
                   children: [
-                    { text: "推送", link: "/docs/extension-push" },
+                    { text: "导入", link: "/docs/extension-import" },
                     { text: "导出", link: "/docs/extension-export" },
-                  ],
+                    { text: "推送", link: "/docs/extension-push" }
+                  ]
                 },
                 {
                   text: "内网部署",
-                  link: "/docs/storage",
+                  link: "/docs/storage"
                 },
-                { text: "常见问题", link: "/docs/FAQ" },
-              ],
+                { text: "常见问题", link: "/docs/FAQ" }
+              ]
             },
             {
               text: "联系我们",
-              link: "/docs/contact",
-            },
+              link: "/docs/contact"
+            }
           ]
-        },
-      },
-      
+        }
+      }
+
       // "/en/": {
       //   selectText: "Languages",
       //   label: "English",
@@ -128,16 +129,16 @@ export default {
     docsBranch: "main",
     repo: "eolinker/eoapi",
     logo: "/images/logo.svg",
-    editLinks: true,
+    editLinks: true
   },
   srcExclude: ["README.md"],
   vue: {
     template: {
       compilerOptions: {
-        isCustomElement: (tag) => tag === "iconpark-icon",
-      },
-    },
-  },
+        isCustomElement: (tag) => tag === "iconpark-icon"
+      }
+    }
+  }
   // vite: {
   //   plugins: [WindiCSS()]
   // },
