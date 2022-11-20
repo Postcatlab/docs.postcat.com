036713fe3012eb48781fd85d1349610ae0e8e47a
scarqin <1054139596@qq.com>
2022-11-20 15:53:52 +0800

    fix: build error

--- user/.vitepress/lib/zoom-image.js
+++ user/.vitepress/lib/zoom-image.js
@@ -1,12 +1,14 @@
-window.addEventListener("load", function (event) {
-  let observer = new MutationObserver(() => {
-    mediumZoom(document.querySelectorAll("main img"), {
-      background: "rgba(0, 0, 0, 0.5)",
-    });
+const zoomImage=() => {
+  mediumZoom(document.querySelectorAll("main img"), {
+    background: "rgba(0, 0, 0, 0.5)",
   });
-  let options = {
-    childList: true,
-    subtree: true,
-  };
-  observer.observe(document.getElementById("app"), options);
-});
+}
+zoomImage();
+let observer = new MutationObserver(zoomImage);
+let options = {
+  childList: true,
+  subtree: true,
+};
+observer.observe(document.getElementById("app"), options);
+
+
