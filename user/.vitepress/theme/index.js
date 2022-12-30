a413adbe9b11ecf4ea45135ee418805e0574f9ab
scarqin <1054139596@qq.com>
2022-12-30 15:32:39 +0800

    fix: download build error

--- user/.vitepress/theme/index.js
+++ user/.vitepress/theme/index.js
@@ -1,13 +1,18 @@
 // .vitepress/theme/index.js
 import DefaultTheme from "vitepress/theme";
 import "./custom.css";
-import "../lib/setDownload.js";
 import { inBrowser, useRoute } from "vitepress";
 import { nextTick, watch } from "vue";
 import mediumZoom from "medium-zoom";
 
 export default {
   ...DefaultTheme,
+  async enhanceApp({ app }) {
+    //Client only import script
+    if (!import.meta.env.SSR) {
+      import("../lib/setDownload.js");
+    }
+  },
   setup() {
     const route = useRoute();
     watch(
